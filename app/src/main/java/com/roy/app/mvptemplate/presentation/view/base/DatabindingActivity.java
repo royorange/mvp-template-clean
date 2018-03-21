df43840a2ce48a6ee0459e35aa243d595ceb50dd
royorange <arvato@Roy.local>
2018-03-21 11:19:31 +0800

    modify databinding activity to abstract

--- app/src/main/java/com/roy/app/mvptemplate/presentation/view/base/DatabindingActivity.java
+++ app/src/main/java/com/roy/app/mvptemplate/presentation/view/base/DatabindingActivity.java
@@ -6,7 +6,7 @@ import android.databinding.ViewDataBinding;
  * Created by Roy on 2018/3/1.
  */
 
-public class DatabindingActivity<T extends ViewDataBinding> extends BaseActivity<CommonPresenter,T> {
+public abstract class DatabindingActivity<T extends ViewDataBinding> extends BaseActivity<CommonPresenter,T> {
 
     @Override
     protected int getContentViewId() {
